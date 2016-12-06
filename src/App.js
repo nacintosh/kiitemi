75b5db441d82e7701d43424a68f054f44571347b
Kazuhito NAKAMURA <kazuhito.nakamura@users.noreply.github.com>
2016-12-07 00:02:31 +0900

    play a youtube content

--- src/App.js
+++ src/App.js
@@ -1,20 +1,42 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
+import YouTube from 'react-youtube';
 
 class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {value: "3ad4NsEy1tg"};
+  }
+
   render() {
+    const opts = {
+      height: '390',
+      width: '640',
+      playerVars: { // https://developers.google.com/youtube/player_parameters
+        autoplay: 1
+      }
+    };
+
     return (
       <div className="App">
         <div className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
-          <h2>Welcome to React</h2>
+          <h2>Welcome to KiiteMi</h2>
         </div>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+        <p className="App-intro"></p>
+        <YouTube
+          videoId={this.state.value}
+          opts={opts}
+          onReady={this._onReady}
+        />
       </div>
     );
+  }
+
+  _onReady(event) {
+    // access to player in all event handlers via event.target
+    event.target.playVideo();
   }
 }
 
